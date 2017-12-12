0eb069b93ad00bf2a4628f5a8270afc1434290a2
“Kudakov <kudakovandrey@gmail.com>
2017-12-12 16:13:05 +0300

    task #48389 Отоброжение полей элементов инфоблока

--- options.php
+++ options.php
@@ -39,7 +39,12 @@ if ($RIGHT >= "R"):
 
     $arMainOptions = array(
         GetMessage("MAIN_OPTIONS"),
-        array("ID" => "ENABLED", "NAME" => GetMessage("LIB_OPTIONS_ENABLED"), "TYPE" => "checkbox", 'SIZE' => false, "DEFAULT" => "Y"),
+        array(
+            "ID" => "ENABLED",
+            "NAME" => GetMessage("LIB_OPTIONS_ENABLED"),
+            "TYPE" => "checkbox",
+            "SIZE" => false,
+            "DEFAULT" => "Y"),
     );
 
     $arIBlockOptions = array();
@@ -50,8 +55,46 @@ if ($RIGHT >= "R"):
         while ($iblock = $arIBlocks->GetNext()) {
             $arIBlockOptions[] = $iblock['NAME'] /*. " ({$iblock['CODE']})"*/;
 
+            $opActive = array();
+            $opActive['ID'] = "iblock.ACTIVE." . $iblock['ID'];
+            $opActive['NAME'] = GetMessage('LIB_OPTIONS_ENABLED');
+            $opActive['TYPE'] = "checkbox";
+            $opActive['DEFAULT'] = false;
+            $arIBlockOptions[] = $opActive;
 
+
+            $arElFields = CIBlockParameters::GetFieldCode(GetMessage("IBLOCK_FIELD"), "LIST_SETTINGS")['VALUES'];
+            $opElFields = array();
+            $opElFields['ID'] = "iblock.ELEMENTFIELDS." . $iblock['ID'];
+            $opElFields['NAME'] = "Поля элементов";
+            $opElFields['TYPE'] = "multiselect";
+            $opElFields['SIZE'] = 10;
+            $opElFields['DEFAULT'] = "";
+            $opElFields['VALUES'] = $arElFields;
+            $arIBlockOptions[] = $opElFields;
+            print_r($opElFields['DEFAULT']);
+
+            $arElProperties = array();
+            $rsProp = CIBlockProperty::GetList(
+                Array("sort" => "asc", "name" => "asc"),
+                Array("ACTIVE" => "Y", "IBLOCK_ID" => $iblock["ID"])
+            );
+            while ($arr = $rsProp->Fetch()) {
+                if (in_array($arr["PROPERTY_TYPE"], array("L", "N", "S", "E"))) {
+                    $arElProperties[$arr["CODE"]] = "[".$arr["CODE"]."] ".$arr["NAME"];
+                }
+            }
+
+            $arSecFields = array();
+
+            $arSecProperties = array();
+            foreach ($USER_FIELD_MANAGER->GetUserFields("IBLOCK_".$iblock["ID"]."_SECTION") as $key => $value) {
+                $arSecProperties[$key] = $key;
+            }
+
+            //\Bitrix\Main\Diag\Debug::dump($arSecProperties);
         }
+
     }
 
 
@@ -60,15 +103,14 @@ if ($RIGHT >= "R"):
 
     CModule::IncludeModule($module_id);
 
-    if($REQUEST_METHOD == "POST" && strlen($Update.$Apply.$RestoreDefaults) > 0 && $RIGHT=="W" && check_bitrix_sessid())
-    {
+    if ($REQUEST_METHOD == "POST" && strlen($Update.$Apply.$RestoreDefaults) > 0 && $RIGHT=="W" && check_bitrix_sessid()) {
         require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/perfmon/prolog.php");
 
         foreach($arMainOptions as $arOption) {
             $name = $arOption['ID'];
             $val = (isset($_REQUEST[$name])) ? $_REQUEST[$name] : "";
 
-            COption::SetOptionString($module_id, $name, $val);
+            //COption::SetOptionString($module_id, $name, $val);
         }
 
         ob_start();
@@ -79,27 +121,45 @@ if ($RIGHT >= "R"):
 
     ?>
 
-    <form method="post" action="<?echo $APPLICATION->GetCurPage()?>?mid=<?=urlencode($module_id)?>&amp;lang=<?=LANGUAGE_ID?>">
+    <form
+        method="post"
+        action="<?echo $APPLICATION->GetCurPage()?>?mid=<?=urlencode($module_id)?>&amp;lang=<?=LANGUAGE_ID?>">
         <?
         $tabControl->Begin();
+
+        /*** MAIN ***/
         $tabControl->BeginNextTab();
+        $tab = "main";
 
         foreach($arMainOptions as $arOption):
             if (is_array($arOption)):
                 $val = COption::GetOptionString($module_id, $arOption['ID'], $arOption['DEFAULT']);
                 $type = $arOption['TYPE'];
+                $id = $tab . "." . $arOption['ID'];
                 ?>
                 <tr>
-                    <td width="40%" nowrap <?if($type == "textarea") echo 'class="adm-detail-valign-top"'?>>
-                        <label for="<?echo htmlspecialcharsbx($arOption['ID'])?>"><?echo $arOption['NAME']?>:</label>
+                    <td width="40%" nowrap <?=($type == "textarea") ? 'class="adm-detail-valign-top"' : ""?>>
+                        <label for="<?=htmlspecialcharsbx($id)?>"><?= $arOption['NAME']?>:</label>
                     </td>
                     <td width="60%">
                         <? if($type == "checkbox"): ?>
-                            <input type="checkbox" name="<?=htmlspecialcharsbx($arOption['ID'])?>" id="<?=htmlspecialcharsbx($arOption['ID'])?>" value="Y"<?if($val=="Y")echo" checked";?>>
+                            <input id="<?=htmlspecialcharsbx($id)?>"
+                                type="checkbox"
+                                name="<?=htmlspecialcharsbx($id)?>"
+                                value="Y" <?=($val == "Y") ? "checked" : ""?>>
                         <? elseif($type == "text"): ?>
-                            <input type="text" size="<?=$arOption['SIZE']?>" maxlength="255" value="<?=htmlspecialcharsbx($val)?>" name="<?=htmlspecialcharsbx($arOption['ID'])?>" id="<?echo htmlspecialcharsbx($arOption['ID'])?>"><?if($arOption[0] == "slow_sql_time") echo GetMessage("PERFMON_OPTIONS_SLOW_SQL_TIME_SEC")?>
+                            <input id="<?= htmlspecialcharsbx($id)?>"
+                                type="text"
+                                size="<?=$arOption['SIZE']?>"
+                                maxlength="255"
+                                value="<?=htmlspecialcharsbx($val)?>"
+                                name="<?=htmlspecialcharsbx($id)?>">
                         <? elseif($type == "textarea"): ?>
-                            <textarea rows="<?=$arOption['ROWS']?>" cols="<?=$arOption['COLS']?>" name="<?echo htmlspecialcharsbx($arOption['ID'])?>" id="<?echo htmlspecialcharsbx($arOption['ID'])?>"><?echo htmlspecialcharsbx($val)?></textarea>
+                            <textarea id="<?= htmlspecialcharsbx($id)?>"
+                                rows="<?=$arOption['ROWS']?>"
+                                cols="<?=$arOption['COLS']?>"
+                                name="<?= htmlspecialcharsbx($id)?>">
+                                <?= htmlspecialcharsbx($val)?></textarea>
                         <? endif; ?>
                     </td>
                 </tr>
@@ -113,24 +173,62 @@ if ($RIGHT >= "R"):
 
         <!--Вкладка с инфоблоками-->
         <? if ($iBlockIncluded): ?>
-            <? $tabControl->BeginNextTab(); ?>
+            <?
+                $tabControl->BeginNextTab();
+                $arIblocksOptions = array();
+            ?>
             <? foreach($arIBlockOptions as $arOption):
                 if (is_array($arOption)): ?>
                     <?
-                        $val = COption::GetOptionString($module_id, $arOption['ID'], $arOption['DEFAULT']);
+                        $id = $arOption['ID'];
                         $type = $arOption['TYPE'];
+                        $val = COption::GetOptionString($module_id, $id, $arOption['DEFAULT']);
                     ?>
                     <tr>
-                        <td width="40%" nowrap <?if($type == "textarea") echo 'class="adm-detail-valign-top"'?>>
-                            <label for="<?echo htmlspecialcharsbx($arOption['ID'])?>"><?echo $arOption['NAME']?>:</label>
+                        <td width="40%" nowrap <?=($type == "textarea") ? 'class="adm-detail-valign-top"' : ""?>>
+                            <label for="<?=htmlspecialcharsbx($id)?>"><?=$arOption['NAME']?>:</label>
                         </td>
                         <td width="60%">
                             <? if($type == "checkbox"): ?>
-                                <input type="checkbox" name="<?=htmlspecialcharsbx($arOption['ID'])?>" id="<?=htmlspecialcharsbx($arOption['ID'])?>" value="Y"<?if($val=="Y")echo" checked";?>>
+                                <input
+                                    type="checkbox"
+                                    name="<?=htmlspecialcharsbx($id)?>"
+                                    id="<?=htmlspecialcharsbx($id)?>"
+                                    value="Y" <?=($val == "Y") ? "checked" : ""?>>
                             <? elseif($type == "text"): ?>
-                                <input type="text" size="<?=$arOption['SIZE']?>" maxlength="255" value="<?=htmlspecialcharsbx($val)?>" name="<?=htmlspecialcharsbx($arOption['ID'])?>" id="<?echo htmlspecialcharsbx($arOption['ID'])?>"><?if($arOption[0] == "slow_sql_time") echo GetMessage("PERFMON_OPTIONS_SLOW_SQL_TIME_SEC")?>
+                                <input
+                                    type="text"
+                                    size="<?=$arOption['SIZE']?>"
+                                    maxlength="255"
+                                    value="<?=htmlspecialcharsbx($val)?>"
+                                    name="<?=htmlspecialcharsbx($id)?>"
+                                    id="<?=htmlspecialcharsbx($id)?>">
                             <? elseif($type == "textarea"): ?>
-                                <textarea rows="<?=$arOption['ROWS']?>" cols="<?=$arOption['COLS']?>" name="<?echo htmlspecialcharsbx($arOption['ID'])?>" id="<?echo htmlspecialcharsbx($arOption['ID'])?>"><?echo htmlspecialcharsbx($val)?></textarea>
+                                <textarea
+                                    rows="<?=$arOption['ROWS']?>"
+                                    cols="<?=$arOption['COLS']?>"
+                                    name="<?=htmlspecialcharsbx($id)?>"
+                                    id="<?=htmlspecialcharsbx($id)?>"><?= htmlspecialcharsbx($val)?></textarea>
+                            <? elseif($type == "multiselect"): ?>
+                                <select
+                                    name="<?=htmlspecialcharsbx($id)?>[]"
+                                    id="<?=htmlspecialcharsbx($id)?>"
+                                    <?=($arOption['SIZE']) ? "size={$arOption['SIZE']}" : ""?>
+                                    multiple>
+                                    <?
+                                    if ($val)
+                                        $arSelectedValues = explode(";;;", $val);
+                                    else
+                                        $arSelectedValues = array();
+                                    ?>
+                                    <option <?=(count($arSelectedValues) > 0) ? "" : "selected"?>>(не выбрано)</option>
+                                    <? foreach ($arOption['VALUES'] as $value => $name): ?>
+                                        <option
+                                            value="<?=$value?>"
+                                            <?=(in_array($value, $arSelectedValues)) ? "selected" : ""?>>
+                                            <?=$name?></option>
+                                    <? endforeach; ?>
+                                </select>
                             <? endif; ?>
                         </td>
                     </tr>
@@ -144,13 +242,32 @@ if ($RIGHT >= "R"):
     <? $tabControl->BeginNextTab();?>
     <? require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/admin/group_rights.php");?>
     <? $tabControl->Buttons();?>
-    <input <?if ($RIGHT<"W") echo "disabled" ?> type="submit" name="Update" value="<?=GetMessage("MAIN_SAVE")?>" title="<?=GetMessage("MAIN_OPT_SAVE_TITLE")?>" class="adm-btn-save">
-    <input <?if ($RIGHT<"W") echo "disabled" ?> type="submit" name="Apply" value="<?=GetMessage("MAIN_OPT_APPLY")?>" title="<?=GetMessage("MAIN_OPT_APPLY_TITLE")?>">
+    <input <?=($RIGHT < "W") ? "disabled" : ""?>
+        type="submit"
+        name="Update"
+        value="<?=GetMessage("MAIN_SAVE")?>"
+        title="<?=GetMessage("MAIN_OPT_SAVE_TITLE")?>"
+        class="adm-btn-save">
+    <input <?=($RIGHT < "W") ? "disabled" : ""?>
+        type="submit"
+        name="Apply"
+        value="<?=GetMessage("MAIN_OPT_APPLY")?>"
+        title="<?=GetMessage("MAIN_OPT_APPLY_TITLE")?>">
     <? if(strlen($_REQUEST["back_url_settings"])>0):?>
-        <input <?if ($RIGHT<"W") echo "disabled" ?> type="button" name="Cancel" value="<?=GetMessage("MAIN_OPT_CANCEL")?>" title="<?=GetMessage("MAIN_OPT_CANCEL_TITLE")?>" onclick="window.location='<?echo htmlspecialcharsbx(CUtil::addslashes($_REQUEST["back_url_settings"]))?>'">
+        <input <?=($RIGHT < "W") ? "disabled" : ""?>
+            type="button"
+            name="Cancel"
+            value="<?=GetMessage("MAIN_OPT_CANCEL")?>"
+            title="<?=GetMessage("MAIN_OPT_CANCEL_TITLE")?>"
+            onclick="window.location='<?echo htmlspecialcharsbx(CUtil::addslashes($_REQUEST["back_url_settings"]))?>'">
         <input type="hidden" name="back_url_settings" value="<?=htmlspecialcharsbx($_REQUEST["back_url_settings"])?>">
     <? endif; ?>
-    <input type="submit" name="RestoreDefaults" title="<?echo GetMessage("MAIN_HINT_RESTORE_DEFAULTS")?>" OnClick="confirm('<?echo AddSlashes(GetMessage("MAIN_HINT_RESTORE_DEFAULTS_WARNING"))?>')" value="<?echo GetMessage("MAIN_RESTORE_DEFAULTS")?>">
+    <input
+        type="submit"
+        name="RestoreDefaults"
+        title="<?echo GetMessage("MAIN_HINT_RESTORE_DEFAULTS")?>"
+        OnClick="confirm('<?echo AddSlashes(GetMessage("MAIN_HINT_RESTORE_DEFAULTS_WARNING"))?>')"
+        value="<?echo GetMessage("MAIN_RESTORE_DEFAULTS")?>">
     <?=bitrix_sessid_post();?>
     <? $tabControl->End();?>
     </form>
